438f7ae7bd14593df1a02460a802a9d4a9e7a2c0
SestoAle <alessandrosestini92@gmail.com>
2021-01-19 09:03:51 +0100

    new experiments

--- agents/PPO.py
+++ agents/PPO.py
@@ -12,8 +12,8 @@ eps = 1e-5
 # Actor-Critic PPO. The Actor is independent by the Critic.
 class PPO:
     # PPO agent
-    def __init__(self, sess, p_lr=1e-7, v_lr=5e-4, batch_fraction=0.33, p_num_itr=20, v_num_itr=10, action_size=19,
-                 epsilon=0.2, c1=0.5, c2=0.01, discount=0.99, lmbda=1.0, name='ppo', memory=10, norm_reward=False,
+    def __init__(self, sess, p_lr=5e-6, v_lr=5e-4, batch_fraction=0.33, p_num_itr=20, v_num_itr=10, action_size=19,
+                 epsilon=0.2, c1=0.5, c2=0.01, discount=0.99, lmbda=1.0, name='ppo', memory=10, norm_reward=True,
                  model_name='agent',
                  **kwargs):
 
