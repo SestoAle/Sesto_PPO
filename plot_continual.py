2c3c97803074019b97a89590a7c77fb89b6e8432
Alessandro Sestini <alessandrosestini92@gmail.com>
2021-01-20 09:28:07 +0100

    update plot continual

--- plot_continual.py
+++ plot_continual.py
@@ -1,19 +1,24 @@
 import matplotlib.pyplot as plt
 import json
 import numpy as np
+import seaborn as sns
+import os
+
+sns.set_theme(style="dark")
 
 import argparse
 import glob
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument('-mn', '--models-name', help="The name of the model", default='ranger_buff*')
+parser.add_argument('-mn', '--models-name', help="The name of the model", default='ranger_def_buf*')
 
 args = parser.parse_args()
 
 plots = args.models_name.split(";")
 
 legends = []
+f, (x1, x2) = plt.subplots(2,1, figsize=(10,6))
 for (i,plot) in enumerate(plots):
 
     plot_title = ''
@@ -27,8 +32,7 @@ for (i,plot) in enumerate(plots):
     if 'entr' in plot:
         plot_title = ' - Entropy'
 
-    plt.figure(i)
-    plt.title("Mean Rewards{}".format(plot_title))
+    #plt.title("Mean Rewards{}".format(plot_title))
     #plt.title("Ranger (trained with DeepCrawl) + Archer (trained with AIRL)")
 
     rewards = []
@@ -47,7 +51,20 @@ for (i,plot) in enumerate(plots):
                 filenames.append(filename)
                 rewards.append(json.load(f))
 
-    print(filenames)
+    try:
+        qual_metrics = []
+        for filename in filenames:
+            dictionary = dict()
+            filename = '{}.txt'.format(filename.replace('.json',''))
+            with open(os.path.join(filename), 'r') as f:
+                for line in f:
+                    if line.split(',')[0] in dictionary:
+                        dictionary[line.split(',')[0]].extend([float(line.split(',')[1])])
+                    else:
+                        dictionary[line.split(',')[0]] = [float(line.split(',')[1])]
+                qual_metrics.append(dictionary)
+    except Exception as e:
+        pass
 
     keys = rewards[0].keys()
 
@@ -68,7 +85,8 @@ for (i,plot) in enumerate(plots):
 
     min_dict['reward_0'] = 0
     max_dict['reward_0'] = 10.
-
+    percentages = []
+    all_data = []
     for k in keys:
         all_rews = []
         data = []
@@ -85,13 +103,41 @@ for (i,plot) in enumerate(plots):
             data.append(np.mean(episode_rewards))
             all_rews.extend(episode_rewards)
 
-        data = np.array(data)
-        data = (data - np.min(all_rews)) / (np.max(all_rews) - np.min(all_rews))
+            if k == 'reward_0':
+                percentages.append(np.sum(np.asarray(episode_rewards) > 0))
 
-        plt.plot(range(len(data)), data, '-o')
+        data = np.array(data)
+        #data = (data - np.min(all_rews)) / (np.max(all_rews) - np.min(all_rews))
+        data = (data - np.min(data)) / (np.max(data) - np.min(data))
+        all_data.append(data)
+
+        x1.plot(range(len(data)), data, '-o', ms=7)
+
         legends.append(k)
 
-    #plt.xticks(np.arange(4), ['$\pi(r1)$', '$\pi(r1+r2)$', '$\pi(r1+r2+r3)$', '$\pi(r1+r3)$'])
-    plt.xticks(np.arange(5), ['$t=0.0$', '$t=1.0$', '$t=0.5$', '$t=0.2$', '$t=0.1$'])
-    plt.legend(legends)
+    x1.set_xticks([])
+    x1.legend(legends)
+
+
+try:
+    # Percentages
+    percentages = []
+    for m in qual_metrics:
+        percentages.append(m['win_rate'][0])
+
+    pal = sns.color_palette("Reds_d", len(percentages))
+    rank = data.argsort().argsort()
+    x = np.array(range(len(percentages)))
+    x2.legend('win rate')
+    sns.barplot(x=x, y=(np.array(percentages)), palette=np.array(pal[::-1])[rank], ax=x2)
+    labels = ['None', 'Mean', 'Mult', 'Entr Thresh', 'Entr Weight', 'From Scratch']
+    x2.set_xticklabels(labels)
+    for p in x2.patches:
+        x2.annotate(format(p.get_height(), '.2f'), (p.get_x() + p.get_width() / 2., p.get_height()), ha = 'center',
+                       va = 'center', xytext = (0, 10), textcoords = 'offset points')
+    plt.setp(x2.patches, linewidth=0)
+except Exception as e:
+    pass
+
+sns.despine()
 plt.show()
