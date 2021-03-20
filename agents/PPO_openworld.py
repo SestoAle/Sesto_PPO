fce9603ceb77fc56b29ca6410d43d89bc441cbe7
SestoAle <alessandrosestini92@gmail.com>
2021-03-20 21:31:45 +0100

    minimal

--- agents/PPO_openworld.py
+++ agents/PPO_openworld.py
@@ -14,7 +14,7 @@ eps = 1e-5
 # Actor-Critic PPO. The Actor is independent by the Critic.
 class PPO:
     # PPO agent
-    def __init__(self, sess, p_lr=5e-5, v_lr=5e-5, batch_fraction=0.33, p_num_itr=20, v_num_itr=10,
+    def __init__(self, sess, p_lr=5e-6, v_lr=5e-6, batch_fraction=0.33, p_num_itr=20, v_num_itr=10,
                  distribution='gaussian', action_type='continuous', action_size=2, action_min_value=-1,
                  action_max_value=1,
                  epsilon=0.2, c1=0.5, c2=0.01, discount=0.99, lmbda=1.0, name='ppo', memory=10, norm_reward=False,
@@ -291,7 +291,7 @@ class PPO:
 
     # Convolutional network, the same for both policy and value networks
     def conv_net(self, global_state, baseline=False):
-
+        global_state = self.linear(global_state, 1024, name='embs', activation=tf.nn.relu)
         return global_state
 
     def sample_batch_for_recurrent(self, length, batch_size):
