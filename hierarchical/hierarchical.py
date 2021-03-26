cc45c96cbefc827831f3e984ebb974af609cfc8c
Alessandro Sestini <alessandrosestini92@gmail.com>
2021-03-26 15:13:22 +0100

    hierc

--- hierarchical/hierarchical.py
+++ hierarchical/hierarchical.py
@@ -34,7 +34,7 @@ class HierarchicalAgent:
         # Instantiate the manager
         self.manager = PPO(
             self.manager_sess, memory=self.manager_memory, p_lr=self.manager_lr,  name='manager', action_size=num_workers,
-            action_type='discrete', action_min_value=0, action_max_value=1
+            action_type='continuous', action_min_value=-1, action_max_value=1
         )
 
         # Instantiate the workers
@@ -100,22 +100,22 @@ class HierarchicalAgent:
 
     def eval_continuous_worker(self, state, man_actions):
         # Get the probability of each worker
-        #man_actions += eps
+        man_actions += eps
         # Softmax
-        #man_actions = np.exp(man_actions) / np.sum(np.exp(man_actions), -1)
-        # probs = np.zeros(self.workers_action_size)
-        # for wg, wk in zip(man_actions, self.workers):
-        #     work_action, work_logprob, work_probs = wk.eval(state)
-        #     probs += (work_probs[0] * wg)
+        man_actions = np.exp(man_actions) / np.sum(np.exp(man_actions), -1)
+        probs = np.zeros(self.workers_action_size)
+        for wg, wk in zip(man_actions, self.workers):
+            work_action, work_logprob, work_probs = wk.eval(state)
+            probs += (work_probs[0] * wg)
         
         #probs /= np.sum(man_actions)
-        #probs = utils.boltzmann(probs, 1)
+        probs = utils.boltzmann(probs, 1)
         # Sample action
         try:
-            #action = np.argmax(np.random.multinomial(1, probs))
-            action, log_probs, probs = self.workers[man_actions].eval(state)
-            action = action[0]
-            probs = probs[0]
+            action = np.argmax(np.random.multinomial(1, probs))
+            # action, log_probs, probs = self.workers[man_actions].eval(state)
+            # action = action[0]
+            # probs = probs[0]
         except Exception as e:
             #print(probs)
             print(e)
