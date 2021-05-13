b51f9e57d4c1c805b24e2547368480654a43c1fa
SestoAle <alessandrosestini92@gmail.com>
2021-05-13 10:02:53 +0200

    models

--- world_model.py
+++ world_model.py
@@ -2,18 +2,26 @@ import numpy as np
 import json
 from PIL import Image
 import matplotlib.pyplot as plt
+import tensorflow as tf
+from motivation.random_network_distillation import RND
+from architectures.bug_arch import *
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+physical_devices = tf.config.experimental.list_physical_devices('GPU')
+if len(physical_devices) > 0:
+    tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
-name = 'bug_detector_gail_schifo_3'
+model_name = 'bug_detector_gail_schifo_3'
 
-with open("arrays/{}.json".format("{}_pos_buffer".format(name))) as f:
+with open("arrays/{}.json".format("{}_pos_buffer".format(model_name))) as f:
     buffer = json.load(f)
 
-with open("arrays/{}.json".format("{}_coverage".format(name))) as f:
+with open("arrays/{}.json".format("{}_coverage".format(model_name))) as f:
     coverage = json.load(f)
 
 trajectories = None
 try:
-    with open("arrays/{}.json".format("{}_trajectories".format(name))) as f:
+    with open("arrays/{}.json".format("{}_trajectories".format(model_name))) as f:
         trajectories = json.load(f)
 except Exception as e:
     pass
@@ -82,24 +90,74 @@ def print_traj(traj):
     plt.xlim(0, 40)
     plt.ylim(0, 40)
     plt.plot(ep_trajectory[:, 0], ep_trajectory[:, 1])
+#
+# # Show last trajectory
+# fig = plt.figure()
+# threshold = 5
+# desired_point_x = 35
+# desired_point_z = 5
+# real_traj = None
+# if trajectories is not None:
+#
+#     for traj in trajectories.values():
+#
+#         traj = np.asarray(traj)
+#         for point in traj:
+#             if np.abs(point[0] - desired_point_x) < threshold and np.abs(point[1] - desired_point_z) < threshold:
+#                 print_traj(traj)
+#                 # real_traj = traj
+#                 break
+#
+# # print(real_traj)
+# # print_traj(real_traj)
 
-# Show last trajectory
-fig = plt.figure()
-threshold = 5
+
+'''
+    Compute the cumulative value of the learnt RND to compare trajectories
+'''
+graph = tf.compat.v1.Graph()
+with graph.as_default():
+    tf.compat.v1.disable_eager_execution()
+    motivation_sess = tf.compat.v1.Session(graph=graph)
+    motivation = RND(motivation_sess, input_spec=input_spec, network_spec=network_spec_rnd,
+                     obs_to_state=obs_to_state_rnd)
+    init = tf.compat.v1.global_variables_initializer()
+    motivation_sess.run(init)
+    motivation.load_model(name=model_name, folder='saved')
+
+filler = np.zeros((42))
+traj_to_observe = []
+rnd_batch = []
 desired_point_x = 35
 desired_point_z = 5
-real_traj = None
+threshold = 5
+moti_rews = []
 if trajectories is not None:
-
     for traj in trajectories.values():
-
-        traj = np.asarray(traj)
         for point in traj:
             if np.abs(point[0] - desired_point_x) < threshold and np.abs(point[1] - desired_point_z) < threshold:
-                print_traj(traj)
-                # real_traj = traj
+                traj_to_observe.append(traj)
                 break
 
-# print(real_traj)
-# print_traj(real_traj)
+    for traj in traj_to_observe:
+        rnd_batch = []
+        for state in traj:
+            state = np.asarray(state)
+            state = 2 * (state/40) - 1
+            state = np.concatenate([state, filler])
+            state = dict(global_in=state)
+            rnd_batch.append(state)
+
+        moti_rew = np.sum(motivation.eval(rnd_batch))
+        moti_rews.append(moti_rew)
+
+    moti_mean = np.mean(moti_rews)
+    print(np.max(moti_rews))
+    idxs_to_observe = np.where(moti_rews > np.asarray(15.))
+    idxs_to_observe = np.reshape(idxs_to_observe, -1)
+    traj_to_observe = np.asarray(traj_to_observe)
+    for traj in traj_to_observe[idxs_to_observe]:
+        print_traj(traj)
+
+
 plt.show()
