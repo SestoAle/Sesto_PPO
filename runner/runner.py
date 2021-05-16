ebc1a4a5f965fc462b27ce18b8aa351499c204a5
Alessandro Sestini <alessandrosestini92@gmail.com>
2021-05-16 12:25:43 +0200

    Update runner.py

--- runner/runner.py
+++ runner/runner.py
@@ -297,6 +297,7 @@ class Runner:
                     # Normalize rewards
                     # intrinsic_rews -= self.reward_model.r_norm.mean
                     # intrinsic_rews /= self.reward_model.r_norm.std
+                    print(intrinsic_rews)
                     print(np.min(intrinsic_rews))
                     print(np.max(intrinsic_rews))
                     intrinsic_rews = (intrinsic_rews - np.min(intrinsic_rews)) / np.max(intrinsic_rews)
