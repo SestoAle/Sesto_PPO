061f079bcb7d18a2c632902673673d2732558620
Alessandro Sestini <alessandrosestini92@gmail.com>
2021-07-18 18:13:36 +0200

    Update world_model_3d_very.py

--- world_model_3d_very.py
+++ world_model_3d_very.py
@@ -334,13 +334,14 @@ if __name__ == '__main__':
                             break
 
             # Cluster trajectories to reduce the number of trajectories to observe
-            traj_to_observe = np.asarray(traj_to_observe)[:2000]
+            traj_to_observe = np.asarray(traj_to_observe)[:1000]
             from copy import deepcopy
             traj_to_cluster = deepcopy(traj_to_observe)
             traj_to_cluster[:, 0] = ((np.asarray(traj_to_cluster[:, 0]) + 1) / 2) * 100
             traj_to_cluster[:, 1] = ((np.asarray(traj_to_cluster[:, 1]) + 1) / 2) * 130
             traj_to_cluster[:, 2] = ((np.asarray(traj_to_cluster[:, 1]) + 1) / 2) * 40
             indexes = np.asarray(cluster_trajectories(traj_to_cluster), np.int)
+            indexes = [ 7,  8,  9,  0,  3, 10, 13,  4,  2,  5, 15,  1,  6, 14, 11, 12, 16]
             traj_to_observe = traj_to_observe[indexes]
 
             # Get the value of the motivation and imitation models of the extracted trajectories
