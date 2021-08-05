9603081cf8e00d929d8cf7bfdc99359faf4d0d2f
Alessandro Sestini <alessandrosestini92@gmail.com>
2021-08-05 11:11:07 +0200

    Revert "Update parallel_runner.py"
    This reverts commit 15ce51d987e3e3eaa267ef42e478bfb2692cbae4.

--- runner/parallel_runner.py
+++ runner/parallel_runner.py
@@ -553,7 +553,6 @@ class Runner:
                         intrinsic_rews -= self.reward_model.r_norm.mean
                         intrinsic_rews /= self.reward_model.r_norm.std
                         print(self.reward_model.r_norm.mean)
-                        input('....')
                     else:
                         intrinsic_rews -= np.mean(intrinsic_rews)
                         intrinsic_rews /= np.std(intrinsic_rews)
@@ -697,7 +696,7 @@ class Runner:
                 print("Reward at the end of episode " + str(ep + 1) + ": " + str(reward))
 
         if isinstance(self.reward_model.r_norm, DynamicRunningStat):
-            rewards = self.reward_model.eval(self.reward_model.policy_traj['obs'], self.reward_model.policy_traj['obs_n'],
+            rewards = self.reward_model.eval(self.reward_model.policy_traj['states'], self.reward_model.policy_traj['states_n'],
                                              self.reward_model.policy_traj['acts'])
 
             for r in rewards:
