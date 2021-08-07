More importance to Action, fix gradient penalty

architectures/bug_arch_really_acc_3d.py
@@ -176,8 +176,8 @@ def network_spec_irl(states, states_n, act, with_action, actions_size):
     #                       )
     threedgrid = tf.cast(tf.reshape(threedgrid, [-1, 15, 15, 15]), tf.int32)
     # threedgrid = tf.reshape(threedgrid, [-1, 15, 15, 15, 1])
-    threedgrid = embedding(threedgrid, indices=2, size=32, name='global_embs')
-    threedgrid = conv_layer_3d(threedgrid, 32, [3, 3, 3], strides=(2, 2, 2), name='conv_01', activation=tf.nn.relu)
+    threedgrid_state = embedding(threedgrid, indices=2, size=32, name='global_embs')
+    threedgrid = conv_layer_3d(threedgrid_state, 32, [3, 3, 3], strides=(2, 2, 2), name='conv_01', activation=tf.nn.relu)
     #threedgrid = tf.nn.max_pool3d(threedgrid, [2, 2, 2], strides=(2, 2, 2), padding="VALID")
     threedgrid = conv_layer_3d(threedgrid, 32, [3, 3, 3], strides=(2, 2, 2), name='conv_02', activation=tf.nn.relu)
     #threedgrid = tf.nn.max_pool3d(threedgrid, [2, 2, 2], strides=(2, 2, 2), padding="VALID")
@@ -198,8 +198,9 @@ def network_spec_irl(states, states_n, act, with_action, actions_size):
     # global_state = tf.reshape(global_state, [-1, 3*280])
 
 
-    action_state = embedding(action_state, indices=10, size=32, name='action_embs')
-    action_state = tf.reshape(action_state, [-1, 32])
+    action_state = embedding(action_state, indices=10, size=512, name='action_embs')
+    action_state = tf.reshape(action_state, [-1, 512])
+    action = action_state
     # action_state = linear(action_state, 64, name='latent_action_n', activation=tf.nn.relu,
     #                       init=tf.compat.v1.keras.initializers.Orthogonal(gain=np.sqrt(2), seed=None,
     #                                                                       dtype=tf.dtypes.float32)
@@ -214,7 +215,7 @@ def network_spec_irl(states, states_n, act, with_action, actions_size):
     #                                                                       dtype=tf.dtypes.float32)
     #                       )
 
-    encoded = tf.concat([threedgrid, action_state], axis=1)
+    encoded = tf.concat([threedgrid, action], axis=1)
 
     global_state = linear(encoded, 1024, name='latent_1', activation=tf.nn.relu,
 
@@ -242,4 +243,4 @@ def network_spec_irl(states, states_n, act, with_action, actions_size):
 
 
 
-    return global_state, encoded
+    return global_state, threedgrid_state, action_state

reward_model/reward_model.py
@@ -615,7 +615,7 @@ class GAIL(RewardModel):
                 self.states, self.act, self.states_n = input()
 
                 with tf.compat.v1.variable_scope('net'):
-                    self.reward, self.latent = network(states=self.states, states_n=self.states_n, act=self.act,
+                    self.reward, _ = network(states=self.states, states_n=self.states_n, act=self.act,
                                                    with_action=self.with_action, actions_size=self.actions_size)
 
                 self.discriminator = tf.nn.sigmoid(self.reward)
@@ -635,10 +635,11 @@ class GAIL(RewardModel):
                     self.expert_states_n = tf.compat.v1.placeholder(tf.float32, [None, length], name='exp_state_n')
 
                     with tf.compat.v1.variable_scope('net', reuse=True):
-                        logits, latent = network(states=[self.expert_states], states_n=[self.expert_states_n], act=self.expert_acts,
+                        net_output = network(states=[self.expert_states], states_n=[self.expert_states_n], act=self.expert_acts,
                                                            with_action=self.with_action, actions_size=self.actions_size)
 
-                    grad_tfs = tf.gradients(logits, latent)
+                    grad_tfs = tf.gradients(net_output[0], net_output[1,:])
+                    grad_tfs = [np.reshape(grad, [BS, -1]) for grad in grad_tfs]
                     grad_tf = tf.concat(grad_tfs, axis=-1)
                     norm_tf = tf.reduce_sum(tf.square(grad_tf), axis=-1)
                     loss_tf = 0.5 * tf.reduce_mean(norm_tf)
@@ -758,7 +759,7 @@ class GAIL(RewardModel):
             acts = np.expand_dims(acts, axis=1)
             feed_dict[self.act] = acts
 
-        rew, latent = self.sess.run([self.discriminator, self.latent], feed_dict=feed_dict)
+        rew = self.sess.run([self.discriminator], feed_dict=feed_dict)
         rew = np.reshape(rew, (-1))
 
         # Reward from original GAIL
@@ -771,4 +772,4 @@ class GAIL(RewardModel):
             for r in rew:
                 self.r_norm.push(r)
 
-        return rew, latent
+        return rew