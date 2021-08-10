2c5622a81eb20e38fd9ba13590a1dd5332d71239
Alessandro Sestini <alessandrosestini92@gmail.com>
2021-08-10 10:06:51 +0200

    Update parallel_runner.py

--- runner/parallel_runner.py
+++ runner/parallel_runner.py
@@ -560,8 +560,8 @@ class Runner:
                     # intrinsic_rews /= self.reward_model.r_norm.std
 
                     #intrinsic_rews = (intrinsic_rews - np.min(intrinsic_rews)) / (np.max(intrinsic_rews) - np.min(intrinsic_rews))
-                    intrinsic_rews -= np.mean(intrinsic_rews)
-                    intrinsic_rews /= (np.std(intrinsic_rews) + 1e-5)
+                    # intrinsic_rews -= np.mean(intrinsic_rews)
+                    # intrinsic_rews /= (np.std(intrinsic_rews) + 1e-5)
                     #intrinsic_rews -= self.reward_model_mean
                     #intrinsic_rews /= self.reward_model_std
                     intrinsic_rews *= self.reward_model.reward_model_weight
