Fixed RND with obs normalization and target diff from predictor

architectures/bug_arch_really_acc_3d.py
@@ -70,7 +70,7 @@ def obs_to_state_rnd(obs):
     global_batch = np.stack([state['global_in'] for state in obs])
     return [global_batch]
 
-def network_spec_rnd(states):
+def network_spec_rnd_predictor(states):
     with_circular = False
 
     global_state = states[0]
@@ -96,27 +96,10 @@ def network_spec_rnd(states):
 
     # global_state = embedding(global_state, indices=280, size=32, name='embs')
     # global_state = tf.reshape(global_state, (-1, 3 * 32))
-    global_state = linear(global_state, 1024, name='global_embs', activation=tf.nn.relu)
+    global_state = linear(global_state, 1024, name='global_embs', activation=tf.nn.leaky_relu)
 
-    # inventory = linear(inventory, 32, name='inventory_embs', activation=tf.nn.tanh)
-    # inventory = linear(inventory, 64, name='inventory_latent', activation=tf.nn.relu)
 
-    # threedgrid = tf.cast(tf.reshape(threedgrid, [-1, 21, 21, 21]), tf.int32)
-    # # threedgrid = tf.reshape(threedgrid, [-1, 15, 15, 15, 1])
-    # threedgrid_state = embedding(threedgrid, indices=3, size=32, name='global_embs')
-    # threedgrid = conv_layer_3d(threedgrid_state, 32, [3, 3, 3], strides=(2, 2, 2), name='conv_01',
-    #                            activation=tf.nn.relu)
-    # # threedgrid = tf.nn.max_pool3d(threedgrid, [2, 2, 2], strides=(2, 2, 2), padding="VALID")
-    # threedgrid = conv_layer_3d(threedgrid, 32, [3, 3, 3], strides=(2, 2, 2), name='conv_02', activation=tf.nn.relu)
-    # # threedgrid = tf.nn.max_pool3d(threedgrid, [2, 2, 2], strides=(2, 2, 2), padding="VALID")
-    # threedgrid = conv_layer_3d(threedgrid, 64, [3, 3, 3], strides=(2, 2, 2), name='conv_03', activation=tf.nn.relu)
-    # threedgrid = conv_layer_3d(threedgrid, 64, [3, 3, 3], strides=(2, 2, 2), name='conv_04', activation=tf.nn.relu)
-    # threedgrid = tf.reshape(threedgrid, [-1, 2 * 2 * 2 * 64])
-
-    #global_state = tf.concat([global_state, inventory], axis=1)
-    #global_state = global_state
-
-    global_state = linear(global_state, 1024, name='latent_1', activation=tf.nn.relu,
+    global_state = linear(global_state, 1024, name='latent_1', activation=tf.nn.leaky_relu,
 
                          )
 
@@ -134,7 +117,43 @@ def network_spec_rnd(states):
 
     return global_state
 
+def network_spec_rnd_target(states):
+    with_circular = False
 
+    global_state = states[0]
+
+    # agent, goal, rays, obs = tf.split(global_state, [4, 3, 12, 21], axis=1)
+    # Jump
+    agent_plane_x, agent_plane_z, agent_jump, is_grounded, can_double_jump, target_distances, goal, threedgrid, rotation, rays, \
+    inventory = \
+        tf.split(global_state, [1, 1, 1, 1, 1, 3, 2, 9261, 4, 12, 2], axis=1)
+
+    # agent_plane_x = ((agent_plane_x + 1) / 2) * 220
+    # agent_plane_x = tf.cast(agent_plane_x, tf.int32)
+    #
+    # agent_plane_z = ((agent_plane_z + 1) / 2) * 280
+    # agent_plane_z = tf.cast(agent_plane_z, tf.int32)
+    #
+    # agent_jump = ((agent_jump + 1) / 2) * 40
+    # agent_jump = tf.cast(agent_jump, tf.int32)
+
+    agent = tf.concat([agent_plane_x, agent_plane_z, agent_jump], axis=1)
+
+    global_state = agent
+
+    # global_state = embedding(global_state, indices=280, size=32, name='embs')
+    # global_state = tf.reshape(global_state, (-1, 3 * 32))
+    global_state = linear(global_state, 1024, name='global_embs', activation=tf.nn.leaky_relu)
+
+
+    global_state = linear(global_state, 1024, name='latent_1', activation=tf.nn.leaky_relu,
+
+                         )
+
+    global_state = linear(global_state, 64, name='out',
+                          )
+
+    return global_state
 
 def input_spec_irl():
     input_length = 9289

architectures/bug_arch_very_acc.py
@@ -68,20 +68,20 @@ def network_spec_rnd(states):
     agent_plane_x, agent_plane_z, agent_jump, is_grounded, can_double_jump, goal, grid, rays, inventory = \
         tf.split(global_state, [1, 1, 1, 1, 1, 5, 49, 12, 2], axis=1)
 
-    agent_plane_x = ((agent_plane_x + 1) / 2) * 220
-    agent_plane_x = tf.cast(agent_plane_x, tf.int32)
-
-    agent_plane_z = ((agent_plane_z + 1) / 2) * 280
-    agent_plane_z = tf.cast(agent_plane_z, tf.int32)
-
-    agent_jump = ((agent_jump + 1) / 2) * 40
-    agent_jump = tf.cast(agent_jump, tf.int32)
+    # agent_plane_x = ((agent_plane_x + 1) / 2) * 220
+    # agent_plane_x = tf.cast(agent_plane_x, tf.int32)
+    #
+    # agent_plane_z = ((agent_plane_z + 1) / 2) * 280
+    # agent_plane_z = tf.cast(agent_plane_z, tf.int32)
+    #
+    # agent_jump = ((agent_jump + 1) / 2) * 40
+    # agent_jump = tf.cast(agent_jump, tf.int32)
 
     agent = tf.concat([agent_plane_x, agent_plane_z, agent_jump], axis=1)
     global_state = agent
 
-    global_state = embedding(global_state, indices=280, size=32, name='embs')
-    global_state = tf.reshape(global_state, (-1, 3 * 32))
+    # global_state = embedding(global_state, indices=280, size=32, name='embs')
+    # global_state = tf.reshape(global_state, (-1, 3 * 32))
     global_state = linear(global_state, 1024, name='global_embs', activation=tf.nn.relu)
 
     # global_state = tf.concat([global_state, inventory], axis=1)

main_bug_really_acc.py
@@ -368,7 +368,8 @@ if __name__ == "__main__":
         with graph.as_default():
             tf.compat.v1.disable_eager_execution()
             motivation_sess = tf.compat.v1.Session(graph=graph)
-            motivation = RND(motivation_sess, input_spec=input_spec, network_spec=network_spec_rnd, lr=7e-5,
+            motivation = RND(motivation_sess, input_spec=input_spec, network_spec_predictor=network_spec_rnd_predictor,
+                             network_spec_rnd_target= network_spec_rnd_target, lr=7e-5,
                              obs_to_state=obs_to_state_rnd, num_itr=30, motivation_weight=1)
             init = tf.compat.v1.global_variables_initializer()
             motivation_sess.run(init)

motivation/random_network_distillation.py
@@ -10,13 +10,14 @@ eps = 1e-12
 
 class RND:
     # Random Network Distillation class
-    def __init__(self, sess, input_spec, network_spec, obs_to_state, lr=7e-5, buffer_size=1e5, batch_size=128,
-                 motivation_weight=1.,
+    def __init__(self, sess, input_spec, network_spec_target, network_spec_predictor, obs_to_state, lr=7e-5, buffer_size=1e5, batch_size=128,
+                 motivation_weight=1., obs_normalization=True,
                  num_itr=3, name='rnd', **kwargs):
 
         # Used to normalize the intrinsic reward due to arbitrary scale
         self.r_norm = RunningStat()
-        #self.obs_norm = RunningStat(shape=(1359))
+        self.obs_norm = RunningStat(shape=(9289))
+        self.obs_normalization = obs_normalization
 
         # The tensorflow session
         self.sess = sess
@@ -28,7 +29,8 @@ class RND:
         self.num_itr = num_itr
         # Functions that define input and network specifications
         self.input_spec = input_spec
-        self.network_spec = network_spec
+        self.network_spec_target = network_spec_target
+        self.network_spec_predictor = network_spec_predictor
         self.obs_to_state = obs_to_state
         # Weight of the reward output by the motivation model
         self.motivation_weight = motivation_weight
@@ -43,12 +45,12 @@ class RND:
             # Target network, it must remain fixed during all the training
             with tf.compat.v1.variable_scope('target'):
                 # Network specification from external function
-                self.target = self.network_spec(self.inputs)
+                self.target = self.network_spec_target(self.inputs)
 
             # Predictor network
             with tf.compat.v1.variable_scope('predictor'):
                 # Network specification from external function
-                self.predictor = self.network_spec(self.inputs)
+                self.predictor = self.network_spec_predictor(self.inputs)
 
             # For fixed target labels, use a placeholder in order to NOT update the target network
             _, latent = shape_list(self.target)
@@ -68,6 +70,14 @@ class RND:
     # Fit function
     def train(self):
         losses = []
+
+        # If we want to use observation normalization, normalize the buffer
+        if self.obs_normalization:
+            print(self.buffer[0]['global_in'])
+            self.normalize_buffer()
+            print(self.buffer[0]['global_in'])
+            input('...')
+
 
         for it in range(self.num_itr):
 
@@ -105,6 +115,14 @@ class RND:
 
     # Eval function
     def eval(self, obs):
+
+        # Normalize observation
+        if self.obs_normalization:
+            print(obs)
+            self.normalize_states(obs)
+            print(obs)
+            input('...')
+
         # Convert the observation to states
         states = self.obs_to_state(obs)
 
@@ -132,6 +150,11 @@ class RND:
 
     # Eval function
     def eval_latent(self, obs):
+
+        # Normalize observation
+        if self.obs_normalization:
+            self.normalize_states(obs)
+
         # Convert the observation to states
         states = self.obs_to_state(obs)
 
@@ -177,7 +200,7 @@ class RND:
             else:
                 del self.buffer[0]
 
-        #self.obs_norm.push(obs['global_in'])
+        self.obs_norm.push(obs['global_in'])
 
         self.buffer.append(obs)
 
@@ -200,6 +223,12 @@ class RND:
     # Normalize the buffer state based on the running mean
     def normalize_buffer(self):
         for state in self.buffer:
+            state['global_in'] = (state['global_in'] - self.obs_norm.mean) / (self.obs_norm.std + eps)
+            state['global_in'] = np.clip(state['global_in'], -5, 5)
+
+    # Normalize input states based on the running mean
+    def normalize_states(self, states):
+        for state in states:
             state['global_in'] = (state['global_in'] - self.obs_norm.mean) / (self.obs_norm.std + eps)
             state['global_in'] = np.clip(state['global_in'], -5, 5)
 

world_model_3d_really.py
@@ -215,8 +215,19 @@ if __name__ == '__main__':
     # Open the trajectories file, if exists. A trajectory is a list of points (& inventory) encountered during training
     trajectories = None
     try:
-        with open("arrays/{}.json".format("{}_trajectories".format(model_name))) as f:
-            trajectories = json.load(f)
+        trajectories = dict()
+        actions = dict()
+        for filename in os.listdir("arrays/{}/".format(model_name)):
+            if 'trajectories' in filename:
+                with open("arrays/{}/{}".format(model_name, filename), 'r') as f:
+                    trajectories.update(json.load(f))
+            else:
+                with open("arrays/{}/{}".format(model_name, filename), 'r') as f:
+                    actions.update(json.load(f))
+
+        # do your stuff
+        # with open("arrays/{}.json".format("{}_trajectories".format(model_name))) as f:
+        #     trajectories = json.load(f)
 
     except Exception as e:
         print("traj problem")
@@ -233,14 +244,14 @@ if __name__ == '__main__':
             pass
 
     # As well as the action made in the episode
-    actions = None
-    try:
-        with open("arrays/{}.json".format("{}_actions".format(model_name))) as f:
-            actions = json.load(f)
-    except Exception as e:
-        print("act problem")
-        print(e)
-        pass
+    # actions = None
+    # try:
+    #     with open("arrays/{}.json".format("{}_actions".format(model_name))) as f:
+    #         actions = json.load(f)
+    # except Exception as e:
+    #     print("act problem")
+    #     print(e)
+    #     pass
 
     # Create pos_buffer from trajectories
     buffer = trajectories_to_pos_buffer(trajectories)
@@ -348,17 +359,17 @@ if __name__ == '__main__':
             # Get only those trajectories that touch the desired points
             for keys, traj in zip(trajectories.keys(), trajectories.values()):
 
-                # to_observe = False
-                # for point in traj:
-                #     de_point = np.zeros(2)
-                #     de_point[0] = ((np.asarray(point[0]) + 1) / 2) * 220
-                #     de_point[1] = ((np.asarray(point[1]) + 1) / 2) * 280
-                #     if np.abs(de_point[0] - 180) < threshold and \
-                #             np.abs(de_point[1] - 116) < threshold:
-                #         to_observe = True
-                #         break
-                #
-                # if to_observe:
+                to_observe = False
+                for point in traj:
+                    de_point = np.zeros(2)
+                    de_point[0] = ((np.asarray(point[0]) + 1) / 2) * 220
+                    de_point[1] = ((np.asarray(point[1]) + 1) / 2) * 280
+                    if np.abs(de_point[0] - 180) < threshold and \
+                            np.abs(de_point[1] - 116) < threshold:
+                        to_observe = True
+                        break
+
+                if to_observe:
                     for i, point in enumerate(traj):
                         de_point = np.zeros(3)
                         de_point[0] = ((np.asarray(point[0]) + 1) / 2) * 220
@@ -463,7 +474,7 @@ if __name__ == '__main__':
 
             # Get those trajectories that have an high motivation reward AND a low imitation reward
             # moti_to_observe = np.where(moti_rews > np.asarray(0.30))
-            sum_moti_rews_dict = {k: v for k, v in sorted(sum_moti_rews_dict.items(), key=lambda item: item[1], reverse=False)}
+            sum_moti_rews_dict = {k: v for k, v in sorted(sum_moti_rews_dict.items(), key=lambda item: item[1], reverse=True)}
             moti_to_observe = [k for k in sum_moti_rews_dict.keys()]
             moti_to_observe = np.reshape(moti_to_observe, -1)
 